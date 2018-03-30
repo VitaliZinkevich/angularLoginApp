4df41081c8590a1936b13e5b9bc867de7e7683f2
vitalizinkevich <vitalizinkevich@gmail.com>
2018-03-30 17:39:06 +0300

    init adn part 1

---
+++ src/app/auth.service.ts
@@ -0,0 +1,13 @@
+import { Injectable } from '@angular/core';
+
+@Injectable()
+export class AuthService {
+
+  constructor() { }
+
+
+    getUserDetails(){
+// post these detalis to api and return user info if correct
+
+    }
+}
